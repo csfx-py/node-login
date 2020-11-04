e6e4a077e3a2f77b1e204c70be7e455e8ed0ffcd
Shreyas <csfxshreyas@gmail.com>
2020-11-04 19:00:52 +0530

    Added level 1 encryption (server DB)

--- app.js
+++ app.js
@@ -1,29 +1,79 @@
 //jshint esversion:6
+
+//----------declarations----------
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-    
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/loginDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
 const app = express();
-const PORT = process.env.PORT || 3000
-    
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-    
-app.get('/', (req,res) => {
+
+const userSchema = {
+    email: String,
+    pass: String,
+};
+
+const User = new mongoose.model("User", userSchema);
+
+//----------get routes----------
+app.get('/', (req, res) => {
     res.render("home");
 });
 
-app.get('/login', (req,res) => {
+app.get('/login', (req, res) => {
     res.render("login");
 });
 
-app.get('/register', (req,res) => {
+app.get('/register', (req, res) => {
     res.render("register");
 });
-    
+
+//----------post routes----------
+app.post('/register', (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        pass: req.body.password,
+    })
+
+    newUser.save((err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("secrets");
+        }
+    })
+});
+
+app.post('/login', (req, res) => {
+    const userName = req.body.username;
+    const passWord = req.body.password;
+
+    User.findOne({ email: userName }, (err, foundUser) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser.pass === passWord) {
+                res.render("secrets");
+            } else {
+                console.log("wrong pass");
+            }
+        }
+    })
+});
+
+//----------listen----------
 app.listen(PORT, () => {
     console.log("Listening on port 3000");
 });
